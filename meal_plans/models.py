d7cfe4fda7a79c9a3620e90fb68e5b7cedc416e7
Ally <allygoose93@gmail.com>
2022-08-31 15:21:26 -0700

    created meal plan app and completed mealplan class and registered it in the admin.py

---
+++ meal_plans/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.conf import settings
+
+USER_MODEL = settings.AUTH_USER_MODEL
+
+# Create your models here.
+class MealPlan(models.Model):
+    name = models.CharField(max_length=120)
+    date = models.DateField(null=True)
+    owner = models.ForeignKey(
+        USER_MODEL,
+        related_name="meal_plan",
+        on_delete=models.CASCADE,
+        null=True,
+    )
+    recipes = models.ManyToManyField("recipes.Recipe", related_name="meal_plans")
+    
