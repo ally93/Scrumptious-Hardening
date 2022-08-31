3a5a7a4d05ade62e87b1c6d17abe7e8e09d19c7f
Ally <allygoose93@gmail.com>
2022-08-31 16:50:49 -0700

    creating views and making its path in urls.py in meal_plans

--- meal_plans/urls.py
+++ meal_plans/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path
 
 from meal_plans.views import (
-    MealPlanListView
+    MealPlanListView,
+    MealPlanCreateView,
+    MealPlanDetailView,
 )
 
 urlpatterns = [
-    path("", MealPlanListView.as_view(), name="meal_plans_list"),
-    # path("<int:pk>/", RecipeDetailView.as_view(), name="recipe_detail"),
+    path("", MealPlanListView.as_view(), name="meal_plan_list"),
+    path("<int:pk>/", MealPlanDetailView.as_view(), name="meal_plan_detail"),
     # path("<int:pk>/delete/", RecipeDeleteView.as_view(), name="recipe_delete"),
-    # path("new/", RecipeCreateView.as_view(), name="recipe_new"),
+    path("new/", MealPlanCreateView.as_view(), name="meal_plan_new"),
     # path("<int:pk>/edit/", RecipeUpdateView.as_view(), name="recipe_edit"),
     # path("<int:recipe_id>/ratings/", log_rating, name="recipe_rating"),
 ]
--- meal_plans/views.py
+++ meal_plans/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
+from django.views.generic.detail import DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from meal_plans.models import MealPlan
 
@@ -10,3 +13,19 @@ from meal_plans.models import MealPlan
 class MealPlanListView(ListView):
     model = MealPlan
     template_name = "meal_plans/list.html"
+
+
+class MealPlanCreateView(LoginRequiredMixin, CreateView):
+    model = MealPlan
+    template_name = "meal_plans/new.html"
+    fields = ["name", "date", "recipes"]
+    success_url = reverse_lazy("meal_plans_list")
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
+
+class MealPlanDetailView(DetailView):
+    model = MealPlan
+    template_name = "meal_plans/detail.html"
