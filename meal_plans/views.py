cd747c304e60cfb6725847a164e02919beff62a9
Ally <allygoose93@gmail.com>
2022-08-31 18:23:32 -0700

    cleaned up list html

--- meal_plans/views.py
+++ meal_plans/views.py
@@ -10,16 +10,19 @@ from meal_plans.models import MealPlan
 # Create your views here.
 
 
-class MealPlanListView(ListView):
+class MealPlanListView(LoginRequiredMixin, ListView):
     model = MealPlan
     template_name = "meal_plans/list.html"
+
+    def get_queryset(self):
+        return MealPlan.objects.filter(owner=self.request.user)
 
 
 class MealPlanCreateView(LoginRequiredMixin, CreateView):
     model = MealPlan
     template_name = "meal_plans/new.html"
     fields = ["name", "date", "recipes"]
-    success_url = reverse_lazy("meal_plans_list")
+    success_url = reverse_lazy("meal_plan_list")
 
     def form_valid(self, form):
         form.instance.owner = self.request.user
@@ -34,10 +37,10 @@ class MealPlanUpdateView(LoginRequiredMixin, UpdateView):
     model = MealPlan
     template_name = "meal_plans/edit.html"
     fields = ["name", "date", "owner", "recipes"]
-    success_url = reverse_lazy("recipes_list")
+    success_url = reverse_lazy("meal_plan_list")
 
 
 class MealPlanDeleteView(LoginRequiredMixin, DeleteView):
     model = MealPlan
     template_name = "meal_plans/delete.html"
-    success_url = reverse_lazy("recipes_list")
+    success_url = reverse_lazy("meal_plan_list")
