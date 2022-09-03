7cbafd69dc27526b20c42336e605b9d1136e5b3a
Ally <allygoose93@gmail.com>
2022-09-03 14:43:42 -0700

    made a model in the new app

---
+++ meal_plans/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.conf import settings
+
+USER_MODEL = settings.AUTH_USER_MODEL
+
+# Create your models here.
+class MealPlan(models.Model):
+    name = models.CharField(max_length=120)
+    date = models.DateField()
+    owner = models.ForeignKey(
+        USER_MODEL,
+        related_name='mealplan',
+        on_delete=models.CASCADE,
+        null=True
+    )
+    recipes = models.ManyToManyField(
+        "recipes.Recipe",
+        related_name="meal_plans"
+    )
