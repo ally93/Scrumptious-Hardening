5c926cbadfff6c486ba2dea483717fcf7604934b
Ally <allygoose93@gmail.com>
2022-09-04 00:03:45 -0700

    edited the list.html to include code from detail.html to make the recipes in a mealplan to show up under the mealplans

--- meal_plans/urls.py
+++ meal_plans/urls.py
@@ -12,7 +12,9 @@ urlpatterns = [
     path("", MealPlanListView.as_view(), name="meal_plan_list"),
     path("<int:pk>/", MealPlanDetailView.as_view(), name="meal_plan_detail"),
     path("new/", MealPlanCreateView.as_view(), name="meal_plan_new"),
-    path("<int:pk>/edit/", MealPlanUpdateView.as_view, name="meal_plan_update"),
+    path(
+        "<int:pk>/edit/", MealPlanUpdateView.as_view(), name="meal_plan_update"
+    ),
     path(
         "<int:pk>/delete/",
         MealPlanDeleteView.as_view(),
